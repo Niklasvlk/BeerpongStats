fac30e630943b81997f2f87e4d517f4ce298f168
Niklas Völker <n.voelker@hhu.de>
2026-03-27 16:37:44 +0100

    Erst ab 10 Spielen wird man im Leaderboard angezeigt.

--- src/main/java/de/niklasvoelker/beerpongstats/repository/PlayerRepository.java
+++ src/main/java/de/niklasvoelker/beerpongstats/repository/PlayerRepository.java
@@ -2,6 +2,8 @@ package de.niklasvoelker.beerpongstats.repository;
 
 import de.niklasvoelker.beerpongstats.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +14,8 @@ public interface PlayerRepository extends JpaRepository<Player, Long> {
 
     Optional<Player> findByName(String name);
 
-    List<Player> findTop10ByWinsPlusLossesGreaterThanEqualOrderByWinsDesc(int minGames);
+    @Query("SELECT p FROM Player p WHERE (p.wins + p.losses) >= :minGames ORDER BY p.wins DESC")
+    List<Player> findLeaderboard(@Param("minGames") int minGames);
 
     List<Player> findByNameContainingIgnoreCase(String name);
 
